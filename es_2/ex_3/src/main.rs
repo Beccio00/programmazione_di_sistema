517cf0dcaee1a6c69bcda5f2e5d5632a77c1cccc
Beccio00 <riccardo.becciolini00@gmail.com>
2025-03-16 17:55:30 +0100

    ex_3

--- es_2/ex_3/src/main.rs
+++ es_2/ex_3/src/main.rs
@@ -1,9 +1,59 @@
+use std::fs;
+use std::error::Error;
+use clap::Parser;
+use std::io;
 
-fn run_program() {
-    
+#[derive(Parser, Debug)]
+struct Args {
+    input: String,
 }
 
 
-fn main() {
-    run_program();
+
+
+fn run_program(filename: &str) -> io::Result<()> {
+    match fs::read_to_string(filename) {
+        
+        Ok(content) => {
+            println!("File letto correttamente:\n{}", content);
+            let repeated_content = content.repeat(10);
+            fs::write(filename, repeated_content)?;
+            println!("File aggiornato con il contenuto ripetuto 10 volte!");
+        }
+        
+        Err(e) => {
+            eprintln!("Errore nel leggere il file: {}", e);
+            return Err(e);
+        }
+    }
+
+    let prefix = String::from("read_");
+
+    let filename_read = format!("{}{}", prefix, filename);
+
+    match fs::read(filename_read) {
+        
+        Ok(content) => {
+            println!("File letto correttamente:\n{}", content);
+            let repeated_content = content.repeat(10);
+            fs::write(filename, repeated_content)?;
+            println!("File aggiornato con il contenuto ripetuto 10 volte!");
+        }
+        
+        Err(e) => {
+            eprintln!("Errore nel leggere il file: {}", e);
+            return Err(e);
+        }
+    }
+
+    Ok(())
+}
+
+
+
+
+fn main()  {
+    let args = Args::parse();
+
+    run_program(&args.input);
 }
