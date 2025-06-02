561a444be2ca96ffd8d2767a99412f4270989ded
Beccio00 <riccardo.becciolini00@gmail.com>
2025-06-03 00:46:54 +0200

    feat(es_7/ex_3): end implementation CyclicBarrier

--- es_7/ex_3/src/lib.rs
+++ es_7/ex_3/src/lib.rs
@@ -1,37 +1,61 @@
 pub mod cyclic_barrier {
-    use std::sync::{mpsc::{sync_channel, Receiver, SyncSender}, Mutex};
+    use std::sync::{mpsc::{channel, sync_channel, Receiver, Sender}, Mutex};
 
     pub struct Waiter {
         receiver: Receiver<()>,
-        senders: Vec<SyncSender<()>>,
-    }
-    
-    pub struct CyclicBarrier{
-        waiteras: Mutex<Vec<Waiter>>,
-        n_threads: usize,
-
+        senders: Vec<Sender<()>>,
     }
 
     impl Waiter {
-        fn new() -> Self {
-            unimplemented!();
-        }
-
-        fn wait() {
-            unimplemented!();
+        pub fn wait(&self) {
+            self.senders.iter().for_each(|s| {
+                s.send(()).unwrap();
+            });
+    
+            for _ in 0..self.senders.len() {
+                self.receiver.recv().unwrap();
+            }
         }
     }
-
+    
+    
+    pub struct CyclicBarrier{
+        receivers: Vec<Receiver<()>>,
+        seners: Vec<Sender<()>>
+    }
 
     impl CyclicBarrier {
-        fn new(n: usize) -> Self {
-            unimplemented!();
+        pub fn new(n: usize) -> Self {
+            let mut senders = Vec::new();
+            let mut recivers = Vec::new();
+
+            for _ in 0..n {
+                let (tx, rx) = channel();
+
+                senders.push(tx);
+                recivers.push(rx);    
+
+            }
+
+            CyclicBarrier { receivers: recivers, seners: senders }
         }
 
-        fn get_waiter(&self) -> Waiter {
-            unimplemented!();
+        pub fn get_waiter(&mut self) -> Result<Waiter, ()> {
+            match self.receivers.len() {
+                0 => {
+                    Err(())
+                },
+                _ => {
+                    let idx = self.seners.len() - self.receivers.len();
+                    let receiver = self.receivers.pop().unwrap();
+                    let senders = self.seners.iter().enumerate()
+                        .filter(|(i,_)| *i != idx )
+                        .map(|(_,s)| s.clone()).collect();
+                    
+                    Ok(Waiter { receiver: receiver, senders: senders })
+                }
+            }
         }
     }
-
 
 }
--- es_7/ex_3/src/main.rs
+++ es_7/ex_3/src/main.rs
@@ -1,14 +1,17 @@
-
+use ex_7_3::cyclic_barrier::CyclicBarrier;
 fn main() {
-    let cbarrrier = CyclicBarrier::new(3);
+    let mut cbarrrier = CyclicBarrier::new(3);
     let mut vt = Vec::new();
     for i in 0..3 {
-        let waiter = cbarrrier.get_waiter();
+        let waiter = cbarrrier.get_waiter().unwrap();
         vt.push(std::thread::spawn(move || {
             for j in 0..10 {
                 waiter.wait();
                 println!("after barrier {} {}", i, j);
             }
         }));
+    }
+    for t in vt {
+        t.join().unwrap();
     }
 }
