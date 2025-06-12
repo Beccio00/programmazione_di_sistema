10a8c3db73ff19b2fcf552a7240de3494215875e
Beccio00 <riccardo.becciolini00@gmail.com>
2025-06-12 13:39:17 +0200

    feat: add ex_2 and ex_3

--- exams/exam_06_24/src/lib.rs
+++ exams/exam_06_24/src/lib.rs
@@ -26,7 +26,7 @@ pub mod ex_1 {
     // dato che appunto perchè in res viene salvato un interatore che conitiene gli elementi del vettore con modulo 2 (2,4,8) che vengono zippati con le
     // lettere dell'alfabeto quindi (a,b,c). Però non viene fatto la collect(), quindi dato che zip è solo un adattore, l'interatore non viene consumato
     // e res rimane un interatore e non viene trasformato in altri tipi.
-    // In last viene salvato invece una copia dell'iteratore a cui viene cambiato il formato quindi mettendo prima 
+    // In last viene salvato invece una copia dell'iteratore a cui viene cambiato il formato quindi mettendo prima
     // la lettera poi il numero e poi viene preso l'ultimo elemento.
     // Se si ometesse la riga 10 last prenderebbe il possesso di res e lo consuma, di
     // conseguenza l'ultima riga andrebbe in constrasto con il borrow checker dato che res non esiste più. Invece con .clone() viene fatta una deep copy
@@ -47,4 +47,125 @@ pub mod ex_1 {
     }
 }
 
-pub mod ex_2 {}
+// Si descriva il comportamento di questo programma.
+// Se presenta delle problematiche, come può essere modificato?
+
+/// ```
+/// use std::sync::{Arc, Condvar, Mutex};
+/// use std::thread;
+/// use std::time::Duration;
+/// fn main() {
+///     let pair = Arc::new((Mutex::new(false), Condvar::new()));
+///     let pair2 = Arc::clone(&pair);
+///     thread::spawn(move || {
+///         let (lock, cvar) = &*pair2;
+///         let mut started = lock.lock().unwrap();
+///         *started = true;
+///         cvar.notify_one();
+///     });
+///     let (lock, cvar) = &*pair;
+///     println!("Waiting ...");
+///     thread::sleep(Duration::from_secs(1));
+///     let mut started = lock.lock().unwrap();
+///     started = cvar.wait(started).unwrap();
+///     println!("End!");
+/// }
+pub mod ex_2 {
+    // Nel codice vengono gestiti due thread, che accedono a un variabile condivisa booleana e comunicanano tra di loro attraverso una condvar.
+    // Il primo è il thread principale che come tale non ho ha bisongo di essere spawnato in quanto lo fa già il main. Il seconod thread viene spawnato
+    // e prende in possesso di clone di Arc che punta a una tupla formata dal Mutex della variabile booleana e Condavar. Il suo compito è quello di
+    // modificare la variabile start e impostarla a true e poi svegliare il thread principale con notify_one(), così da simulare il fatto che il thread
+    // ha iniziato la sua esecuzione e avvisare ciò attraverso il mutex. Il thread principaale invece prima esegue sleep per un secondo, simulando
+    // l'escuzione successivamente esgue wait su il mutexguard che condivisono i due thread. Quindi il thread svegliara il thread principale nel momento
+    // in cui finisce. In questo caso ci possono essere delle notifiche supurie e quindi sterted non è stato modificato e quindi è comunque false, per come		
+    // come è stato implementato il codice non c'è nessuno controllo quindi il thread prinicpale pensa che il secondo thread abbia già fatto quello che d
+    // doveva fare. Per miglioare il codice basta sostuire wait(started) con wait_while(!*started) in modo che anche se viene svegliato ma started è ancora       
+    // false allora rimane in attesa.
+
+    use std::sync::{Arc, Condvar, Mutex};
+    use std::thread;
+    use std::time::Duration;
+    pub fn run_ex_2() {
+        let pair = Arc::new((Mutex::new(false), Condvar::new()));
+        let pair2 = Arc::clone(&pair);
+        thread::spawn(move || {
+            let (lock, cvar) = &*pair2;
+            let mut started = lock.lock().unwrap();
+            *started = true;
+            cvar.notify_one();
+        });
+        let (lock, cvar) = &*pair;
+        println!("Waiting ...");
+        thread::sleep(Duration::from_secs(1));
+        let mut started = lock.lock().unwrap();
+        started = cvar.wait_while(started, |s| !*s).unwrap();
+        println!("End!");
+    }
+}
+
+//Il codice seguente genera un errore di compilazione: spiegare perché e indicare come
+//modificare la struct S (attraverso l'aggiunta di tratti) per renderlo compilabile ed eseguibile.
+///```
+///#[derive(Debug)]
+///struct S {
+///    i: i32,
+///}
+///impl From<i32> for S {
+///    fn from(value: i32) -> Self {
+///        S { i: value }
+///    }
+///}
+///fn main() {
+///    let mut v = Vec::<S>::new();
+///    let s = 42.into();
+///    for i in 0..3 {
+///        v.push(s);
+///    }
+///    println!("{:?}", v);
+///}
+
+pub mod ex_3 {
+	// Il problema di questo codice è che in s inzialmente è in possesso di una variabile tipo S che non implementa il tratto Copy. Nel momento in cui
+	// viene fatto v.push(s) la prima volta avviene con successo e la variabile s viene mossa all'interno dell'heap dove punta v, consumando s.
+	// al secondo passaggio del ciclo in s non esiste più e v.push e il borrow cheker darà errore di compilazione. Per renderlo compatibile con la
+	// la logica implementata basterebbe far si che struct S derivi anche i tratti Clone e Copy. In questo modo quando viene eseguito v.push 
+	// non viene mossa la variabile s ma viene inserita in v una copia di S.
+	
+	#[derive(Debug, Clone, Copy)]
+	struct S {
+	    i: i32,
+	}
+	impl From<i32> for S {
+	    fn from(value: i32) -> Self {
+		S { i: value }
+	    }
+	}
+	pub fn run_ex_3() {
+	    let mut v = Vec::<S>::new();
+	    let s = 42.into();
+	    for i in 0..3 {
+		v.push(s);
+	    }
+	    println!("{:?}", v);
+	}
+}
+
+//      Si realizzi l’implementazione della struttura dati Exchanger<T: Send> (e dei metodi e delle
+//     funzioni necessarie) utile per realizzare una comunicazione bidirezionale.
+///     Ciascun lato della comunicazione dispone di un’istanza della struttura [`Exchanger<T: Send>.`]
+//     La comunicazione avviene invocando il metodo
+///```
+///     fn exchange(&self, t:T) -> Option<T>
+//     che, una volta invocato, si blocca fino a quando non viene invocato il metodo corrispettivo
+//     sulla struttura corrispondente al lato opposto della comunicazione, dopodiché restituisce il
+//     valore che è stato passato come argomento al metodo corrispondente al lato opposto (che
+//     farà altrettanto), sotto forma di Some(t).
+//     Lo scambio può essere ripetuto un numero arbitrario di volte.
+//     Se una delle due strutture formanti la coppia viene distrutta, un'eventuale chiamata, bloccata
+//     sul metodo della struttura restante, terminerà restituendo il valore None.
+//     Si implementi tale struttura in linguaggio Rust avendo cura che la sua implementazione
+//     sia thread-safe
+
+pub mod ex_4 {
+
+}
--- exams/exam_06_24/src/main.rs
+++ exams/exam_06_24/src/main.rs
@@ -1,5 +1,9 @@
 use exam_06_24::ex_1::run_ex_1;
+use exam_06_24::ex_2::run_ex_2;
+use exam_06_24::ex_3::run_ex_3;
 
 fn main() {
     run_ex_1();
+    run_ex_2();
+    run_ex_3();
 }
